f3b7571651b0e82c5d6a59c39a7fb284305cd187
David De Santiago <107174944+ddesantiag0@users.noreply.github.com>
2022-10-13 19:49:05 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,25 +1,41 @@
 import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
-    int num = 0;
-    ArrayList<e> = 
+    ArrayList<String> words = new ArrayList<String>();
+    ArrayList<String> lookup = new ArrayList<String>();
+
     public String handleRequest(URI url) {
+
+        words.clear();
+        lookup.clear();
+        
         if (url.getPath().equals("/")) {
-            return String.format("Search Engine", num);
-        } else if (url.getPath().equals("/add")) {
-            num += 1;
-            return String.format("Number incremented!");
+            return String.format("Search Engine");
+            
         } else {
-            System.out.println("Search Engine " + url.getPath());
+            System.out.println("Path: " + url.getPath());
             if (url.getPath().contains("/add")) {
                 String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("")) {
-                    num += Integer.parseInt(parameters[1]);
-                    return String.format("", parameters[1], num);
+                if (parameters[0].equals("s")) {
+                    words.add(parameters[1]);
+                    return String.format("Added: %s", parameters[1]);
+                }
+            }
+            if (url.getPath().contains("/search")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s")) {
+                    for (int i = 0; i < words.size(); i++) {
+                        if (words.get(i).contains(parameters[1])) {
+                            lookup.add(words.get(i));
+                        }
+                    }
+                    return String.format(Arrays.toString(lookup.toArray()));
+
                 }
             }
             return "404 Not Found!";
@@ -30,17 +46,12 @@ class Handler implements URLHandler {
 class SearchEngine {
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
-            System.out.println("Invalid Web Link, please try again");
+            System.out.println("Missing port number, try any number between 1024 to 49151");
             return;
         }
-        else{
-            for(int i = 0; i < args.length; i++){
-                
-            }
-        }
+        
+        int val = Integer.parseInt(args[0]);
 
-        int port = Integer.parseInt(args[0]);
-
-        Server.start(port, new Handler());
+        Server.start(val, new Handler());
     }
 }
